c9f898fca442d3a142c01907582dd5e054ab65d3
appcompat-wx <appcompatwx@microsoft.com>
2026-04-23 13:32:00 +0800

    upload retarget code from lab

---
+++ ModernFlyouts/Controls/VolumeControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ModernFlyouts.Controls
+{
+    public partial class VolumeControl : UserControl
+    {
+        public VolumeControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
